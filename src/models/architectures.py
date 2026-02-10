011198274a08d767d5561c4620a8551747a6eab2
Leonardo Lucas da Silva <leonardo3443@gmail.com>
2026-02-10 11:54:11 -0300

    Updated LSTM builder to avoid Tensor Flow warnings

--- src/models/architectures.py
+++ src/models/architectures.py
@@ -21,10 +21,27 @@ def build_lstm_seq_to_vec(horizon, timesteps, num_features, activation = 'tanh',
         Compiled LSTM sequence-to-vector model.
     """
 
+
     model = tf.keras.Sequential([
-        tf.keras.layers.LSTM(64, activation=activation, return_sequences=True, input_shape=(timesteps, num_features)),
-        tf.keras.layers.LSTM(48, activation=activation, return_sequences=True),
-        tf.keras.layers.LSTM(32, activation=activation, return_sequences=False),
-        tf.keras.layers.Dense(horizon, kernel_initializer=kernel_initializer)])
+        tf.keras.Input(shape=(timesteps, num_features)),
+
+        tf.keras.layers.LSTM(
+            64,
+            activation=activation,
+            return_sequences=True
+        ),
+        tf.keras.layers.LSTM(
+            48,
+            activation=activation,
+            return_sequences=True
+        ),
+        tf.keras.layers.LSTM(
+            32,
+            activation=activation
+        ),
+
+        tf.keras.layers.Dense(horizon, kernel_initializer=kernel_initializer)
+    ])
+
     return model
 
