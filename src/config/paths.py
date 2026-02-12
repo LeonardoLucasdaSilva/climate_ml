ede026ca6b6cdbec8e04cd2ca6387776fbe431a0
Leonardo Lucas da Silva <leonardo3443@gmail.com>
2026-02-12 15:33:37 -0300

    Added new path representation for images of graphs

--- src/config/paths.py
+++ src/config/paths.py
@@ -7,8 +7,10 @@ DATA_DIR = PROJECT_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
 INTERIM_DATA_DIR = DATA_DIR / "interim"
 PROCESSED_DATA_DIR = DATA_DIR / "processed"
+IMAGE_DATA_DIR = DATA_DIR / "images"
 
 # Create the directories if they don't exist
 RAW_DATA_DIR.mkdir(parents=True, exist_ok=True)
 INTERIM_DATA_DIR.mkdir(parents=True, exist_ok=True)
 PROCESSED_DATA_DIR.mkdir(parents=True, exist_ok=True)
+IMAGE_DATA_DIR.mkdir(parents=True, exist_ok=True)
