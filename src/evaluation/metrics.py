1c7ee8869d55ed4480b8de7e00a7af906da1f955
Leonardo Lucas da Silva <leonardo3443@gmail.com>
2026-02-13 01:58:27 -0300

    Added a function for SMAPE

--- src/evaluation/metrics.py
+++ src/evaluation/metrics.py
@@ -31,3 +31,9 @@ def mae(y_true, y_pred):
 
     return np.mean(np.abs(y_true - y_pred))
 
+def smape(y_true, y_pred):
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
+
+    denominator = (np.abs(y_true) + np.abs(y_pred)) / 2
+    return np.mean(np.abs(y_true - y_pred) / denominator) * 100
