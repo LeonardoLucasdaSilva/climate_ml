6a95c5d3ecb00655966e651f0ea20fe7744e5b24
Leonardo Lucas da Silva <leonardo3443@gmail.com>
2026-02-16 16:33:59 -0300

    Added column concatenation by mean

--- src/data/prepare.py
+++ src/data/prepare.py
@@ -1,6 +1,6 @@
 import xarray as xr
 
-def concat_precipitation(raw_path, interim_path, timespan = '1D', overwrite = False):
+def sum_column(raw_path, interim_path, timespan = '1D', overwrite = False):
 
     # Timespan values
     # 1D - daily
@@ -20,3 +20,18 @@ def concat_precipitation(raw_path, interim_path, timespan = '1D', overwrite = Fa
     concat_dataset = nc_dataset.resample(valid_time=timespan).sum()
 
     concat_dataset.to_netcdf(interim_path)
+
+def mean_column(raw_path, interim_path, timespan='1D', overwrite=False):
+
+    if interim_path.exists() and not overwrite:
+        print(f"File {interim_path} already exists")
+        return
+
+    nc_dataset = xr.open_dataset(raw_path, engine="netcdf4")
+
+    # Convert Total Precipitation (m) to (mm)
+    nc_dataset["tp"].values = nc_dataset["tp"].values * 1000
+
+    mean_dataset = nc_dataset.resample(valid_time=timespan).mean()
+
+    mean_dataset.to_netcdf(interim_path)
