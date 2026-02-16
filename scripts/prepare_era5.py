fa61a80a42fb0d9927c15ffc487775bf464c8b93
Leonardo Lucas da Silva <leonardo3443@gmail.com>
2026-02-16 16:34:34 -0300

    Added column concatenation by mean and clear code definition of which variables go in each case

--- scripts/prepare_era5.py
+++ scripts/prepare_era5.py
@@ -1,4 +1,4 @@
-from src.data.prepare import concat_precipitation
+from src.data.prepare import sum_column, mean_column
 import pandas as pd
 from src.config.paths import RAW_DATA_DIR, INTERIM_DATA_DIR
 
@@ -8,19 +8,27 @@ def main():
     df = pd.read_csv('../data/inmet_pos.csv')
     df.columns = ['cidade', 'lat', 'lon']
 
-    for index, row in df.iterrows():
+    era5_columns = ["2m_dewpoint_temperature", "surface_pressure", "surface_solar_radiation_downwards",
+                    "2m_temperature", "total_precipitation"]
+    era5_columns_mean = ["2m_dewpoint_temperature", "surface_pressure", "2m_temperature"]
+    era5_columns_sum = ["surface_solar_radiation_downwards", "total_precipitation"]
 
-        timespan = '1D'
-        raw_filename = "era5_precipitation_timeseries_"+row['cidade'].lower()+".nc"
-        interim_filename = "era5_precipitation_timeseries_" + row['cidade'].lower() + "_" + timespan + ".nc"
+    for column in era5_columns:
+        for index, row in df.iterrows():
 
-        print(RAW_DATA_DIR / raw_filename)
+            timespan = '1D'
+            raw_filename = "era5_precipitation_timeseries_"+row['cidade'].lower()+".nc"
+            interim_filename = "era5_precipitation_timeseries_" + row['cidade'].lower() + "_" + timespan + ".nc"
 
-        concat_precipitation(
-            RAW_DATA_DIR / raw_filename,
-            INTERIM_DATA_DIR / interim_filename,
-            timespan
-        )
+            print(RAW_DATA_DIR / raw_filename)
+
+            if column in era5_columns_sum:
+                sum_column(RAW_DATA_DIR / raw_filename, INTERIM_DATA_DIR / interim_filename, timespan)
+            elif column in era5_columns_mean:
+                mean_column(RAW_DATA_DIR / raw_filename, INTERIM_DATA_DIR / interim_filename, timespan)
+            else:
+                raise ValueError(f"Column {column} does not belong to any of the lists: era5_columns_mean, era5_columns_sum")
+
 
 if __name__ == "__main__":
     main()
