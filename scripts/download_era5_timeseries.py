7b463f8057e0ab8b8dd3ff9e925d367e44305424
Leonardo Lucas da Silva <leonardo3443@gmail.com>
2026-02-16 16:31:35 -0300

    Inserted download of multiple ERA5 timeseries columns

--- scripts/download_era5_timeseries.py
+++ scripts/download_era5_timeseries.py
@@ -5,27 +5,29 @@ from src.spatial.grid import latlon_to_grid
 # Read the positions from inmet stations from a csv file and store them in a pandas dataframe
 df = pd.read_csv('../data/inmet_pos.csv')
 df.columns = ['cidade', 'lat', 'lon']
+era5_columns = ["2m_dewpoint_temperature", "surface_pressure", "surface_solar_radiation_downwards", "2m_temperature", "total_precipitation"]
 
 
 # Download the timeseries from all stations given the closest era5 grid point
 DATASETS = []
 
-for index, row in df.iterrows():
+for column in era5_columns:
+    for index, row in df.iterrows():
 
-    lat,lon = latlon_to_grid(row['lat'], row['lon'])
+        lat,lon = latlon_to_grid(row['lat'], row['lon'])
 
-    requisition = {
-        "name": "era5_precipitation_timeseries_"+row['cidade'].lower(),
-        "dataset": "reanalysis-era5-single-levels-timeseries",
-        "request": {
-            "variable": ["total_precipitation"],
-            "location": {"longitude": lon, "latitude": lat},
-            "date": ["1979-01-01/2025-12-31"],
-            "data_format": "netcdf"
-        },
-        "filename": "era5_precipitation_timeseries_"+row['cidade'].lower()
-    }
-    DATASETS.append(requisition)
+        requisition = {
+            "name": "era5_"+column+"_timeseries_"+row['cidade'].lower(),
+            "dataset": "reanalysis-era5-single-levels-timeseries",
+            "request": {
+                "variable": [column],
+                "location": {"longitude": lon, "latitude": lat},
+                "date": ["1979-01-01/2025-12-31"],
+                "data_format": "netcdf"
+            },
+            "filename": "era5_"+column+"_timeseries_"+row['cidade'].lower()
+        }
+        DATASETS.append(requisition)
 
 
 def main():
